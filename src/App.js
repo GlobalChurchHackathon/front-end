05e67ad4b95f617e56fe4904c08e8646a1c55ab2
Kyrune <jn-01@centurytel.net>
2020-03-28 14:04:44 -0500

    started login components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Login from './components/views/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
+      <Login />
     </div>
   );
 }
