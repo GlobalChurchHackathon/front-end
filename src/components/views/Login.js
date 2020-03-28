555ac17fb9fe8b016084139c6de6a2a2b906de61
Kyrune <jn-01@centurytel.net>
2020-03-28 14:25:14 -0500

    Started login component

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class Login extends Component {
     render() {
         return (
             <div>
-                <Form inline>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="examplePassword" className="mr-sm-2">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
-                </FormGroup>
-                <Button styles={{background: "green"}}>Submit</Button>
-            </Form>
+                <Form inline className="login-form">
+                    <h1>Log In</h1>
+                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                        <Label for="email" className="mr-sm-2">Email</Label>
+                        <Input type="email" name="email" id="email" placeholder="email" />
+                    </FormGroup>
+                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                        <Label for="password" className="mr-sm-2">Password</Label>
+                        <Input type="password" name="password" id="password" placeholder="Password" />
+                    </FormGroup>
+                    <Button style={{background: "green"}}>Submit</Button>
+                </Form>
             </div>
         )
     }
-}
+};
