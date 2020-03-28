926c70debbb067ce688d626cbed340d29e6f385d
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-03-28 12:36:12 -0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavBar from './components/layouts/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
+      <NavBar />
     </div>
   );
 }
