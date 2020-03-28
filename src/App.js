66b703f4e9077311529c8b053046795cdbd3ceea
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-03-28 15:22:19 -0700

    added RegisterUser component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navigation from './components/layouts/Navigation';
+import RegisterUser from './components/views/RegisterUser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
       <Navigation />
+      <RegisterUser />
     </div>
   );
 }
