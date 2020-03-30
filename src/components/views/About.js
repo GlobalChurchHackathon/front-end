0afb9b9d024c0827f88853d4071b5a1c85e46c8b
Kyrune <jn-01@centurytel.net>
2020-03-30 18:45:12 -0500

    Added About description

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -10,11 +10,11 @@ const About = () => {
                     <h1 className="display-3 text-center">About</h1>
                     <hr className="my-1"></hr>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-                        officia deserunt mollit anim id est laborum.
+                        In the midst of chaos lies the eye of the storm – a place of peace and assurance that all will be well. 
+                        Our goal is to provide people with a way to connect with essential resources, whether it be food, hygiene, 
+                        or household items, through neighboring churches and communities. If you are in need, if you want a place 
+                        to rest and regain your footing, please create an account, head to the “Need Help?” tab, and choose what 
+                        you need. If you would like to help, you can post your available supplies. All are welcome!
                     </p>
                 </Jumbotron>
             </Container>
