f6211105f37f68c4d996f857ed7a33eed1274a1f
Kyrune <jn-01@centurytel.net>
2020-03-30 13:48:15 -0500

    Created About component

---
+++ src/components/views/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+const About = () => {
+    return (
+        <div>
+            <Container>
+                <Jumbotron style={{background: "white", opacity: "70%"}}>
+                    <h1 className="display-3 text-center">About</h1>
+                    <hr className="my-1"></hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                        officia deserunt mollit anim id est laborum.
+                    </p>
+                </Jumbotron>
+            </Container>
+        </div>
+    );
+}
+
+export default About;
