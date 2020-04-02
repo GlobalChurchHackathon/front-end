e5b0c7a99603f7c532726e2ee8b696389308eba9
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-04-02 12:35:55 -0700

    still a work in progress. Added redux package.json

--- src/components/views/requestedItems/Food.js
+++ src/components/views/requestedItems/Food.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import { Button, Table, DropdownButton, Dropdown, Modal } from 'react-bootstrap';
 
-const Food = () => {
+//drop down, why do you need this item
+//button fireEvent, when fired 'bag' icon number increments 
+
+const items = [
+    {
+        id: 1,
+        product: "meats",
+
+    },
+    {
+        id: 2,
+        product: "grains"
+    },
+    {
+        id: 3,
+        product: "vegtibles"
+    }
+]
+const Food = (userToken) => {
+
+    // const [product, addProduct] = React.useState(null)
+
+    // React.useEffect(() => {
+    //     if (window) {
+    //         product(window(userToken))
+    //     }
+    // }, [userToken])
+
     return (
         <div>
             <h1>Food</h1>
-            <p>House Hold Table or list? will need to figure out how to fire and event to the back end alerting church memebers a request has been made</p>
-        </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>placeholder</th>
+                        <th>Product Name</th>
+                        <th>Add to bag</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <Button style={{ width: '100%', height: '100%', border: '1px solid' }}>Add to bag</Button> */}
+                    {
+                        items.map(item =>
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.product}</td>
+                                <td><Button style={{ width: '100%', height: '100%', border: '1px solid' }}>Add to bag</Button></td>
+                            </tr>
+                        )
+                    }
+                    <tr>
+                        <td style={{ textAlign: "right " }} colspan={3}>Total Items:</td>
+                    </tr>
+
+                    <tr>
+                        <td style={{ textAlign: "right " }} colspan={4}>
+                            <Button>Place Order</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div >
     )
 }
 
