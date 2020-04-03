ba49540fdebb37efafc4c1ab9df2637225677a90
Kyrune <jn-01@centurytel.net>
2020-04-03 07:54:58 -0500

    Added login auth Actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -58,3 +58,35 @@ export const register = ({ name, email, password }) => async dispatch => {
         });
     }
 };
+
+// Login user
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post('api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+          errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    }
+};
