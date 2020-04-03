42190bdceb51f1797783e6e5dbb08d4ec05eed4b
Kyrune <jn-01@centurytel.net>
2020-04-03 15:58:14 -0500

    Added conditional navbar rendering

--- src/components/layouts/Navigation.js
+++ src/components/layouts/Navigation.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 import { Image, Navbar, Nav } from 'react-bootstrap';
 import logo from '../assests/nav-logo.png';
 
-const Navigation = () => {
-    return (
-        <Navbar expand="lg" bg="dark" varient="dark">
-            <Navbar.Brand>
-                <Link to='/'>
-                    <Image src={logo} style={{ maxWidth: "100%", width: "300px", height: "auto", marginLeft: "-.5em" }}></Image>
-                </Link>
-            </Navbar.Brand>
+const Navigation = ({ auth: { isAuthenticated, loading }, logout }) => {
+    // This navbar shows up if user IS logged in
+    const authLinks = (
+        <Fragment>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ background: "white" }} />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/requestHelpPage">Need Help?</Link>
+                    <Link to="/churchRequestBoard">Requests Board</Link>
+                    <Link to="/give">Give</Link>
+                    <a onClick={logout} href='/'>
+                        <span>Logout</span>
+                    </a>
+                </Nav>
+            </Navbar.Collapse>
+        </Fragment>
+    );
+
+    // This navbar shows up if user is NOT logged in
+    const guestLinks = (
+        <Fragment>
             <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ background: "white" }} />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
@@ -23,8 +41,28 @@ const Navigation = () => {
                     <Link to="/give">Give</Link>
                 </Nav>
             </Navbar.Collapse>
+        </Fragment>
+    );
+    return (
+        // Conditional Navigation rendering
+        <Navbar expand="lg" bg="dark" varient="dark">
+            <Navbar.Brand>
+                <Link to='/'>
+                    <Image src={logo} style={{ maxWidth: "100%", width: "300px", height: "auto", marginLeft: "-.5em" }}></Image>
+                </Link>
+            </Navbar.Brand>
+            { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </Navbar>
     );
 }
 
-export default Navigation;
+Navigation.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navigation);
